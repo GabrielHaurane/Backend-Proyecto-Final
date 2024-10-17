0588bccebe3ef25ab2edc0a223159bbd58fbcdbb
Gabriel Alejandro Haurane <165182347+GabrielHaurane@users.noreply.github.com>
2024-10-17 10:32:53 -0300

    Revert "Base"

--- src/database/databaseConnection.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { mongo } from "mongoose";
-
-const mongodb = process.env.MONGODB
-mongoose.connect(mongodb)
-const connexion = mongoose.connection
-
-connexion.once('open', ()=>{
-    console.info('BD Conectada')
-})
