c7588f31a6cc3863249b4631c0c3f140c44e4781
Augusto Brito <britoaugusto1@gmail.com>
2024-10-27 13:29:34 -0300

    Se añadió el middleware para verificarAdmin a las rutas de habitaciones

--- src/routes/habitacion.routes.js
+++ src/routes/habitacion.routes.js
@@ -8,14 +8,15 @@ import {
 } from "../controllers/habitacion.controllers.js";
 import validacionHabitacion from "../helpers/validacionHabitacion.js";
 import verificarJWT from "../helpers/verificaJWT.js";
+import verificarAdmin from "../helpers/verificarAdmin.js";
 const habitacionRouter = Router()
 habitacionRouter
 .route('/habitacion')
-.post([ verificarJWT,validacionHabitacion],crearHabitacion)
+.post([ verificarJWT,validacionHabitacion,verificarAdmin],crearHabitacion)
 .get(listarHabitacion)
 habitacionRouter
 .route('/habitacion/:id')
 .get(obtenerHabitacion)
-.delete(borrarHabitacion)
-.put([ verificarJWT,validacionHabitacion],editarHabitacion)
+.delete([verificarJWT,verificarAdmin],borrarHabitacion)
+.put([ verificarJWT,validacionHabitacion,verificarAdmin],editarHabitacion)
 export default habitacionRouter
