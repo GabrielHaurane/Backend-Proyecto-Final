a50c17d74044ee293b882ebe904292d1bc7b6b3d
Augusto Brito <britoaugusto1@gmail.com>
2024-11-01 09:20:50 -0300

    Se creo un nuevo controllers para filtrar la lista de reservas por el email ingresado por parametro

--- src/routes/reserva.routes.js
+++ src/routes/reserva.routes.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import {
   borrarReserva,
   crearReserva,
-  listarReservas,
+  // listarReservas,
+  listarReservasPorUsuario,
   obtenerReserva,
 } from "../controllers/reserva.controllers.js";
 import verificarJWT from "../helpers/verificaJWT.js";
 
 const reservaRouter = Router();
-reservaRouter.route("/reserva").get([verificarJWT], listarReservas).post([verificarJWT], crearReserva);
+reservaRouter.route("/reserva").get([verificarJWT], listarReservasPorUsuario).post([verificarJWT], crearReserva);
 reservaRouter.route("/reserva/:id").get([verificarJWT],obtenerReserva).delete([verificarJWT],borrarReserva);
 export default reservaRouter;
